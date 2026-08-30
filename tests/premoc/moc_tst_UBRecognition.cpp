84115f21e5a854c4413f112a8ea78500a4d2abec
David Guyomarch <dguyomarch@gmail.com>
2026-08-30 14:09:36 +0200

    chore: add missing premoc files and update .gitignore
    Add 3 missing pre-generated moc files for test suites added in recent
    sessions (UBGraphicsScene, UBRecognition, UBSmoothStrokeItem).
    
    Update 2 existing premoc files regenerated with Qt 6.8.3 moc
    (moc_UBFileSystemUtils.cpp, moc_tst_UBVisualRegression.cpp).
    
    Add visual regression test output (visual/) and generated resource
    file (tests/qrc_visual_tests.cpp) to .gitignore.

---
+++ tests/premoc/moc_tst_UBRecognition.cpp
@@ -0,0 +1,170 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'tst_UBRecognition.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.8.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../tst_UBRecognition.h"
+#include <QtNetwork/QSslError>
+#include <QtCore/qmetatype.h>
+
+#include <QtCore/qtmochelpers.h>
+
+#include <memory>
+
+
+#include <QtCore/qxptype_traits.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'tst_UBRecognition.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.8.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+struct qt_meta_tag_ZN17TestUBRecognitionE_t {};
+} // unnamed namespace
+
+
+#ifdef QT_MOC_HAS_STRINGDATA
+static constexpr auto qt_meta_stringdata_ZN17TestUBRecognitionE = QtMocHelpers::stringData(
+    "TestUBRecognition",
+    "testExtractFromEmptySelection",
+    "",
+    "testExtractSkipsDuplicateStrokes",
+    "testSegmentSingleCharacter",
+    "testSegmentTwoCharactersWithGap",
+    "testSegmentOverlappingStrokesGrouped",
+    "testAdjustedWidthWithZoom",
+    "testComputeZoomRatio",
+    "testTruncateText"
+);
+#else  // !QT_MOC_HAS_STRINGDATA
+#error "qtmochelpers.h not found or too old."
+#endif // !QT_MOC_HAS_STRINGDATA
+
+Q_CONSTINIT static const uint qt_meta_data_ZN17TestUBRecognitionE[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       8,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   62,    2, 0x08,    1 /* Private */,
+       3,    0,   63,    2, 0x08,    2 /* Private */,
+       4,    0,   64,    2, 0x08,    3 /* Private */,
+       5,    0,   65,    2, 0x08,    4 /* Private */,
+       6,    0,   66,    2, 0x08,    5 /* Private */,
+       7,    0,   67,    2, 0x08,    6 /* Private */,
+       8,    0,   68,    2, 0x08,    7 /* Private */,
+       9,    0,   69,    2, 0x08,    8 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject TestUBRecognition::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_ZN17TestUBRecognitionE.offsetsAndSizes,
+    qt_meta_data_ZN17TestUBRecognitionE,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_tag_ZN17TestUBRecognitionE_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<TestUBRecognition, std::true_type>,
+        // method 'testExtractFromEmptySelection'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'testExtractSkipsDuplicateStrokes'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'testSegmentSingleCharacter'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'testSegmentTwoCharactersWithGap'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'testSegmentOverlappingStrokesGrouped'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'testAdjustedWidthWithZoom'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'testComputeZoomRatio'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'testTruncateText'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void TestUBRecognition::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    auto *_t = static_cast<TestUBRecognition *>(_o);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        switch (_id) {
+        case 0: _t->testExtractFromEmptySelection(); break;
+        case 1: _t->testExtractSkipsDuplicateStrokes(); break;
+        case 2: _t->testSegmentSingleCharacter(); break;
+        case 3: _t->testSegmentTwoCharactersWithGap(); break;
+        case 4: _t->testSegmentOverlappingStrokesGrouped(); break;
+        case 5: _t->testAdjustedWidthWithZoom(); break;
+        case 6: _t->testComputeZoomRatio(); break;
+        case 7: _t->testTruncateText(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject *TestUBRecognition::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *TestUBRecognition::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_ZN17TestUBRecognitionE.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int TestUBRecognition::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 8)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 8;
+    }
+    if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 8)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 8;
+    }
+    return _id;
+}
+QT_WARNING_POP
